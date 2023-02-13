dc97ac3b49bb41fb9f4439e0ec36071b25f9d0a2
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2023-02-13 13:47:47 +0100

    start

---
+++ react-test.jsx
@@ -0,0 +1,10 @@
+console.log('works')
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+let number = 0
+console.log(root, number)
+
+root.render(
+    <div>
+        <h1>Hello React</h1>
+    </div>
+)
